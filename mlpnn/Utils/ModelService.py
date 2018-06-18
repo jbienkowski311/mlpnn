71fb364922e658a6ab54591c4edddbde74e74612
Jakub Bieńkowski <jbienkowski311@gmail.com>
2018-06-18 21:05:36 +0200

    Rename mlpnn to model in ModelService

--- mlpnn/Utils/ModelService.py
+++ mlpnn/Utils/ModelService.py
@@ -2,18 +2,18 @@ from pickle import dump, load, HIGHEST_PROTOCOL
 
 
 class ModelService(object):
-    def save(self, mlpnn, file):
-        self._save(mlpnn, file)
+    def save(self, model, file):
+        self._save(model, file)
 
     def load(self, file):
         return self._load(file)
 
-    def _save(self, mlpnn, file):
+    def _save(self, model, file):
         with open(file.path, 'wb') as output:
-            dump(mlpnn, output, HIGHEST_PROTOCOL)
+            dump(model, output, HIGHEST_PROTOCOL)
 
     def _load(self, file):
-        with open(file.path, 'rb') as mlpnn:
-            instance = load(mlpnn)
+        with open(file.path, 'rb') as model:
+            instance = load(model)
 
         return instance
